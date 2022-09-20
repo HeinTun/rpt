90960fe739628b971c3e13d09c7565a0b322a1e7
HeinTun <94649752+HeinTun@users.noreply.github.com>
2022-09-20 15:55:34 -0400

    moved the keys  for deployment

--- secret.py
+++ secret.py
@@ -8,9 +8,6 @@ import streamlit as st
 
 @st.cache
 def im_keys():
-    AWS_ACCESS_KEY = "AKIA4GK7IHHC32JKRZSQ"
     # AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID") <-- better approach; need to figure out how
-    AWS_SECRET_KEY = "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"
-    AWS_REGION = "us-east-1"
-    return (AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_REGION)
+    return (st.secrets["AWS_ACCESS_KEY"], st.secrets["AWS_SECRET_KEY"], "us-east-1")
 
